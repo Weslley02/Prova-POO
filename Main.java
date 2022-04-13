2d442c3cc52723af0af7fed1b4eb2d3759d5ab24
WeslleyBorges3 <80173269+Weslley02@users.noreply.github.com>
2022-04-13 23:43:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    Paciente Paciente1 = New Paciente (Paciente1, medico1, 10);
+    Paciente Paciente1 = new Paciente;
+
+    Quarto Quarto1 = New Quarto (Quarto1, medico1);
+    Quarto Quarto1 = new Paciente;
+
+    Medico Medico1 = New Medico (Quarto, Paciente);
+    Medico Medico1 = New Medico;
+      
+      
+  }
+}
---
+++ Medico.java
@@ -0,0 +1,13 @@
+class Medico{
+  
+    int id;
+    int Medico;
+  
+    Medico1(int id, int Medico){
+      this.id = id;
+      this.Medico = Medico;
+    }
+
+
+    }
+   
